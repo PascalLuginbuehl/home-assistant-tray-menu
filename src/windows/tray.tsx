598a133b72db3b7af1c5ff3e0b5051cbd4891647
Pascal Luginbühl <17087783+PascalLuginbuehl@users.noreply.github.com>
2023-07-26 17:11:47 +0200

    feat: tray icon change color

--- src/windows/tray.tsx
+++ src/windows/tray.tsx
@@ -1,21 +1,19 @@
 import {
   app,
-  ipcMain, Menu, Tray,
+  ipcMain, Menu, nativeTheme, Tray,
 } from 'electron';
 import path from 'path';
 import i18next from '../i18next';
 import { createPanel, showPanel } from './panel-controller';
 import openSettings from './settings';
 import defaultIconImagePath from '../../assets/icon@3x.png';
-import transparentIconImagePath from '../../assets/transparentIcon@3x.png';
-import warningIconImagePath from '../../assets/alert@2x.png';
+import defaultIconDarkImagePath from '../../assets/iconDark@3x.png';
 import errorIconImagePath from '../../assets/redIcon@3x.png';
 import APIUrlStateEnum from '../types/api-state-enum';
 
 const ICON_PATHS = {
   DEFAULT: path.resolve(__dirname, defaultIconImagePath),
-  TRANSPARENT: path.resolve(__dirname, transparentIconImagePath),
-  WARNING_ICON: path.resolve(__dirname, warningIconImagePath),
+  DEFAULT_DARK: path.resolve(__dirname, defaultIconDarkImagePath),
   ERROR: path.resolve(__dirname, errorIconImagePath),
 };
 
@@ -29,7 +27,8 @@ export function createTray() {
     panelWindow.reload();
   });
 
-  tray = new Tray(ICON_PATHS.DEFAULT);
+  tray = new Tray(nativeTheme.shouldUseDarkColors ? ICON_PATHS.DEFAULT : ICON_PATHS.DEFAULT_DARK);
+
   tray.setToolTip('Home Assistant Controlls');
 
   const contextMenu = Menu.buildFromTemplate([
@@ -54,7 +53,7 @@ export function setIconStatus(status: APIUrlStateEnum) {
   }
 
   if (status === APIUrlStateEnum.ok) {
-    tray.setImage(ICON_PATHS.DEFAULT);
+    tray.setImage(nativeTheme.shouldUseDarkColors ? ICON_PATHS.DEFAULT : ICON_PATHS.DEFAULT_DARK);
   } else {
     tray.setImage(ICON_PATHS.ERROR);
   }
